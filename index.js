80a7d7d839f12202c4c3dc9d1eb6226ef97ec71a
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-12-06 21:43:44 +0100

    :tada:

--- index.js
+++ index.js
@@ -1,44 +1,102 @@
-function ppSymbol(value) {
+function repeat(string, n) {
+  if (n <= 0) {
+    return ""
+  }
+  let result = ""
+  for (let i = 0; i < n; i++) {
+    result += string
+  }
+  return result
+}
+
+function getOpts(opts = {}) {
+  const {level = 0, indentation = 2} = opts
+  return {
+    level,
+    indentation
+  }
+}
+
+function pad(string, numWhitespace = 0) {
+  return repeat(" ", numWhitespace) + string
+}
+
+function prettyString(value) {
+  return `"${value}"`
+}
+
+function prettySymbol(value) {
   return value.toString()
 }
 
-function ppString(value) {
-  return value
-}
-
-function ppNumber(value) {
+function prettyNumber(value) {
   return value.toString()
 }
 
-function ppArray(value, opts) {
-  return value.map(v => prettyPrint(v, opts))
+function prettyBoolean(value) {
+  return value.toString()
 }
 
-function ppObject(value, opts) {
-  return Object.keys(value).reduce((agg, key) => {
-    agg[key] = prettyPrint(value[key], opts)
-    return agg
-  }, {})
+function prettyNull(value) {
+  return value === null ?
+    "null" :
+    "undefined"
 }
 
-function prettyPrint(value, opts = {}) {
-  const {indent = true, indentation = 2, shallow = false} = opts
+function prettyArray(value, opts) {
+  const {level, indentation} = getOpts(opts)
+  let result = "[\n"
+  for (let i = 0; i < value.length; i++) {
+    result += pad(prettyPrint(value[i], {
+        level,
+        indentation
+      }), level * indentation) + ",\n"
+  }
+  result = result.substring(0, result.length - 2)
+  result += '\n' + pad("]", (level - 1) * indentation)
+  return result
+}
+
+function prettyObject(value, opts) {
+  const {level, indentation} = getOpts(opts)
+  let result = "{\n"
+  for (key of Object.keys(value)) {
+    result += pad(prettyPrint(key), level * indentation)
+    result += ": "
+    result += prettyPrint(value[key], {
+      level,
+      indentation
+    })
+    result += ",\n"
+  }
+  result = result.substring(0, result.length - 2)
+  result += '\n' + pad("}", (level - 1) * indentation)
+  return result
+}
+
+function prettyPrint(value, opts) {
+  const {level, indentation, result} = getOpts(opts)
   switch (typeof value) {
     case "string":
-      return ppString(value, opts)
-    case "number":
-      return ppNumber(value, opts)
+      return prettyString(value)
+    case "undefined":
+      return prettyNull(value)
     case "symbol":
-      return ppSymbol(value, opts)
+      return prettySymbol(value)
+    case "boolean":
+      return prettyBoolean(value)
+    case "number":
+      return prettyNumber(value)
     case "object":
-      const complexOpts = Object.assign({}, opts, {
-        shallow: true
-      })
-      const parsed = Array.isArray(value) ? ppArray(value, complexOpts) : ppObject(value, complexOpts)
-      if (shallow) {
-        return parsed
+      const nextOpts = {
+        level: level + 1,
+        indentation
       }
-      return JSON.stringify(parsed, null, indentation)
+      return Array.isArray(value) ?
+        prettyArray(value, nextOpts) :
+        value != null ?
+          prettyObject(value, nextOpts) :
+          prettyNull(value)
   }
 }
 
--- test/prettyprint.test.js
+++ test/prettyprint.test.js
@@ -3,37 +3,75 @@ const pp = require('../')
 
 describe("prettyprint", () => {
   it("prints strings", () => {
-    expect(pp("foo")).to.equal("foo")
+    expect(pp("foo")).to.equal("\"foo\"")
   })
 
   it("prints symbols", () => {
     expect(pp(Symbol("foo"))).to.equal("Symbol(foo)")
   })
 
+  it("prints booleans", () => {
+    expect(pp(true)).to.equal("true")
+    expect(pp(false)).to.equal("false")
+  })
+
+  it("prints nil", () => {
+    expect(pp(null)).to.equal("null")
+    expect(pp(undefined)).to.equal("undefined")
+  })
+
   it("prints numbers", () => {
+    expect(pp(-23), "negative int").to.equal("-23")
     expect(pp(3), "int").to.equal("3")
     expect(pp(3.14), "float").to.equal("3.14")
+    expect(pp(-1 / 2), "negative float").to.equal("-0.5")
+    expect(pp(NaN), "nan").to.equal("NaN")
     expect(pp(Infinity), "infinity").to.equal("Infinity")
+    expect(pp(-Infinity), "negative infinity").to.equal("-Infinity")
   })
 
   it("prints arrays", () => {
-    console.log(pp([Symbol("foo"), [Symbol("bar")]]))
+
     expect(pp([Symbol("foo"), [Symbol("bar")]])).to.equal(`[
-  "Symbol(foo)",
+  Symbol(foo),
   [
-    "Symbol(bar)"
+    Symbol(bar)
+  ]
+]`)
+    expect(pp([1, [2, [3, [4], 5], 6]])).to.equal(`[
+  1,
+  [
+    2,
+    [
+      3,
+      [
+        4
+      ],
+      5
+    ],
+    6
   ]
 ]`)
   })
 
   it("prints objects", () => {
-    console.log(pp({
+    expect(pp({
       foo: {
-        baz: "qux",
-        bar: Symbol("bar"),
-        foo: Infinity
+        bar: {
+          baz: {
+            qux: 42
+          }
+        }
       }
-    }))
+    })).to.equal(`{
+  "foo": {
+    "bar": {
+      "baz": {
+        "qux": 42
+      }
+    }
+  }
+}`)
     expect(pp({
       foo: {
         baz: "qux",
@@ -43,9 +81,25 @@ describe("prettyprint", () => {
     })).to.equal(`{
   "foo": {
     "baz": "qux",
-    "bar": "Symbol(bar)",
-    "foo": "Infinity"
+    "bar": Symbol(bar),
+    "foo": Infinity
   }
+}`)
+  })
+
+  it("prints the good stuff", () => {
+    expect(pp({
+      foo: [{
+        bar: [1]
+      }]
+    })).to.equal(`{
+  "foo": [
+    {
+      "bar": [
+        1
+      ]
+    }
+  ]
 }`)
   })
 })
